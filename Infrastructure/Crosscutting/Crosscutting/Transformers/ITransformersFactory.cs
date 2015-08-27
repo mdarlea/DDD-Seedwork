47389b49dc72b24ef788cddfe1cf80773a8894c1
Michelle Darlea <mdarlea@gmail.com>
2015-08-27 07:42:23 -0400

    Added Create method to the Transformer interface

--- Infrastructure/Crosscutting/Crosscutting/Transformers/ITransformersFactory.cs
+++ Infrastructure/Crosscutting/Crosscutting/Transformers/ITransformersFactory.cs
@@ -1,7 +1,10 @@
+using System;
+
 namespace Swaksoft.Infrastructure.Crosscutting.Transformers
 {
     public interface ITransformersFactory<TBase, TDestination>
     {
         ITransformer<TDestination> Create<TSource>() where TSource : TBase;
+        ITransformer<TDestination> Create(Type sourceType);
     }
 }
